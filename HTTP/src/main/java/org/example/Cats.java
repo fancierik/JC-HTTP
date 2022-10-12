3e47e452e572b72aaa5508580c8552c22197332d
Ann Ovcharuk <anblackdiamond@gmail.com>
2022-10-12 14:19:35 +0300

    commit

---
+++ HTTP/src/main/java/org/example/Cats.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cats {
+
+    String id;
+    String text;
+    String type;
+    String user;
+    String upvotes;
+
+    public Cats() {
+    }
+
+    public Cats(@JsonProperty("id") String id,
+                @JsonProperty("text") String text,
+                @JsonProperty("type") String type,
+                @JsonProperty("user") String user,
+                @JsonProperty("upvotes") String upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cats{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes='" + upvotes + '\'' +
+                '}';
+    }
+}
---
+++ HTTP/src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static final String targetUrl = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(targetUrl);
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        List<Cats> facts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+        });
+
+        facts.stream().filter(value -> value.getUpvotes() != null && Integer.parseInt(value.getUpvotes()) > 0)
+                .forEach(System.out::println);
+
+
+    }
+}
